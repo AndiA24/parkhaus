2c00acfc0ffd7c273643dc1c6665fbccd912ec72
AndreasAdler <a.adler.tab@icloud.com>
2026-02-25 13:45:24 +0100

    Add doxygen comments to intit_vehicle and free_vehicle

--- include/vehicle.h
+++ include/vehicle.h
@@ -8,6 +8,16 @@ typedef struct {
     unsigned int queue_time;       /**< Number of simulation steps the vehicle spent waiting in the queue. */
 } Vehicle;
 
+/**
+ * @brief Initializes a Vehicle struct with default values.
+ *
+ * @param ptr_vehicle Pointer to the Vehicle to initialize.
+ */
 void init_vehicle(Vehicle *ptr_vehicle) {}
 
+/**
+ * @brief Frees Memory of a Vehicle after the exit.
+ *
+ * @param ptr_vehicle Pointer to the Vehicle to free.
+ */
 void free_vehicle(Vehicle *ptr_vehicle) {}
