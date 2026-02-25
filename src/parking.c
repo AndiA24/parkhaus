10c8686bc2ce0d96fcf7eadb05ec9bfe7905455c
AndreasAdler <a.adler.tab@icloud.com>
2026-02-25 18:11:11 +0100

    Finish check_exit Function

--- src/parking.c
+++ src/parking.c
@@ -8,21 +8,19 @@ FUNCTION check_exit(ptr_parking, ptr_simstats):
         FOR j <- 0 TO (ptr_parking->ptr_decks[i].capacity - 1):
 
             IF ptr_parking->ptr_decks[i].ptr_spots[j].occupied == 1:
-                Vehicle *ptr_temp_vehicle = ptr_parking->ptr_decks[i].ptr_spots[j].ptr_vehicle
+                ParkingSpot *ptr_spot = (ptr_parking->ptr_decks[i].ptr_spots + j)
+                Vehicle *ptr_vehicle = ptr_spot->ptr_vehicle
 
-                IF sim_step - vehicle->entry_time >= vehicle->parking_duration: // check if parking duration is over
+                IF sim_step - ptr_vehicle->entry_time >= ptr_vehicle->parking_duration: 
                     
-
-                    // Stats aus dem Fahrzeug übertragen (BEVOR free())
-                    ptr_simstats->exits_this_step    += 1
+                    ptr_simstats->temp_exits         += 1
                     ptr_simstats->total_exits        += 1
-                    ptr_simstats->total_queue_time   += vehicle->queue_time
-                    ptr_simstats->total_parking_time += vehicle->parking_duration
+                    ptr_simstats->total_queue_time   += ptr_vehicle->queue_time
+                    ptr_simstats->total_parking_time += ptr_vehicle->parking_duration
 
-                    // Spot freigeben
-                    free_vehicle(ptr_temp_vehicle)
-                    Setze spot.ptr_vehicle = NULL
-                    Setze spot.occupied = 0
+                    free_vehicle(ptr_vehicle)
+                    Setze ptr_spot->ptr_vehicle = NULL
+                    Setze ptr_spot->occupied = 0
 
                     ptr_parking->occupied_count -= 1
 
