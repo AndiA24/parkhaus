9f2cfaea27b8ec85a9341a0ac3d69fea03930331
AndreasAdler <a.adler.tab@icloud.com>
2026-02-25 18:22:35 +0100

    Adapt check_exits to pseudocode-syntax

--- src/parking.c
+++ src/parking.c
@@ -1,28 +1,31 @@
 #include "../include/parking.h"
 
-FUNCTION check_exit(ptr_parking, ptr_simstats):
-    sim_step = ptr_simstats->step_num
+FUNCTION check_exit(ptr_parking, ptr_simstats)
 
-    FOR i <- 0 TO (ptr_parking->decks - 1):
+    sim_step <- ptr_simstats->step_num
 
-        FOR j <- 0 TO (ptr_parking->ptr_decks[i].capacity - 1):
+    FOR i <- 0 TO (ptr_parking->decks - 1) DO
 
-            IF ptr_parking->ptr_decks[i].ptr_spots[j].occupied == 1:
-                ParkingSpot *ptr_spot = (ptr_parking->ptr_decks[i].ptr_spots + j)
-                Vehicle *ptr_vehicle = ptr_spot->ptr_vehicle
+        FOR j <- 0 TO (ptr_parking->ptr_decks[i].capacity - 1) DO
 
-                IF sim_step - ptr_vehicle->entry_time >= ptr_vehicle->parking_duration: 
-                    
-                    ptr_simstats->temp_exits         += 1
-                    ptr_simstats->total_exits        += 1
-                    ptr_simstats->total_queue_time   += ptr_vehicle->queue_time
-                    ptr_simstats->total_parking_time += ptr_vehicle->parking_duration
+            IF ptr_parking->ptr_decks[i].ptr_spots[j].occupied == 1 THEN
 
-                    free_vehicle(ptr_vehicle)
-                    Setze ptr_spot->ptr_vehicle = NULL
-                    Setze ptr_spot->occupied = 0
+                ptr_spot    <- Adresse von ptr_parking->ptr_decks[i].ptr_spots[j]
+                ptr_vehicle <- ptr_spot->ptr_vehicle
 
-                    ptr_parking->occupied_count -= 1
+                IF (sim_step - ptr_vehicle->entry_time) >= ptr_vehicle->parking_duration THEN
+
+                    // Statistiken aktualisieren
+                    ptr_simstats->temp_exits         <- ptr_simstats->temp_exits + 1
+                    ptr_simstats->total_exits        <- ptr_simstats->total_exits + 1
+                    ptr_simstats->total_queue_time   <- ptr_simstats->total_queue_time + ptr_vehicle->queue_time
+                    ptr_simstats->total_parking_time <- ptr_simstats->total_parking_time + ptr_vehicle->parking_duration
+
+                    // Fahrzeug freigeben und Stellplatz leeren
+                    CALL free_vehicle(ptr_vehicle)
+                    ptr_spot->ptr_vehicle <- NULL
+                    ptr_spot->occupied    <- 0
+                    ptr_parking->occupied_count <- ptr_parking->occupied_count - 1
 
                 END IF
 
@@ -31,3 +34,5 @@ FUNCTION check_exit(ptr_parking, ptr_simstats):
         END FOR
 
     END FOR
+
+END FUNCTION
