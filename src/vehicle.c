76715634f3ed9ed77fe49c05dfd55aedaedf651b
Lukas <lukas.kornmayer@web.de>
2026-02-27 01:16:32 +0100

    Implementing pseudocode for create_vehicle function defined in vehicle.h

--- src/vehicle.c
+++ src/vehicle.c
@@ -1,1 +1,18 @@
-#include "../include/vehicle.h"
+/*
+FUNCTION create_vehicle(id : int, entry_time : int, duration : int) RETURNS Vehicle*
+
+    ptr_vehicle : Vehicle*
+    ptr_vehicle = malloc(sizeof(Vehicle))
+
+    IF ptr_vehicle == NULL
+        RETURN NULL
+    ENDIF
+
+    ptr_vehicle->id = id
+    ptr_vehicle->entry_time = entry_time
+    ptr_vehicle->parking_duration = duration
+    ptr_vehicle->queue_time = 0
+
+    RETURN ptr_vehicle
+END FUNCTION
+*/
