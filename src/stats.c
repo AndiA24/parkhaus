da010dc6c79e089a610637916bb345528a034abc
Lukas <lukas.kornmayer@web.de>
2026-03-02 18:45:13 +0100

    Implement logic for function create_output_file to check if file already exists

--- src/stats.c
+++ src/stats.c
@@ -15,12 +15,35 @@ FUNCTION init_simstats()
     return ptr_simstats
 END FUNCTION
 
-FUNCTION create_output_file(config)
-    
+FUNCTION create_output_file(ptr_config : SimConfig*) RETURNS FILE*
+    user_input : char
 
+    DO
+        output_file = fopen(ptr_config->output_file_name, "r")
 
-    return ptr_output_file
+        IF output_file != NULL
+            fclose(output_file)
+            CALL printf("File already exists. Overwrite? (y/n): ")
+            CALL scanf(" %c", &user_input)
 
+            IF user_input == 'y'
+                BREAK
+            ELSE
+                CALL printf("Enter new file name: ")
+                CALL scanf("%s", ptr_config->output_file_name) 
+            ENDIF
+        ELSE
+            BREAK
+        ENDIF
+    WHILE TRUE
+
+    output_file = fopen(ptr_config->output_file_name, "w")
+
+    IF output_file == NULL
+        RETURN NULL
+    ENDIF
+
+    RETURN output_file
 END FUNCTION
 
 FUNCTION update_simstats()
