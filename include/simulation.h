728ce4f68d041941ca3dfd0a3f564da7abac70c3
Andreas Adler <andi@mac.speedport.ip>
2026-03-02 15:19:56 +0100

    Add rand_arrival function

--- include/simulation.h
+++ include/simulation.h
@@ -13,3 +13,14 @@
  * @return                  Returns 1 for sucessful simulation or -1 for failure.
  */
 int run_simulation(SimConfig *ptr_config, SimStats *ptr_stats);
+
+/**
+ * @brief Decides random whether a car arrives or not
+ *
+ * Calls all functions necessary for an simulation step, as often given in the config
+ * 
+ * @param[in]       config  Pointer to the SimConfig structure containing the number of stats
+ *                          and configurations for the simulation (number of spots, decks...)
+ * @return                  Returns 1 for arrival 0 for no arrival
+ */
+int rand_arrival(SimConfig *ptr_config);
