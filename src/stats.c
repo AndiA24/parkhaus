cc51c6b590455aa2a607892de708455287393b9b
Andreas Adler <andi@mac.speedport.ip>
2026-03-02 13:32:42 +0100

    Add first draft for stats functions in pseudocode

--- src/stats.c
+++ src/stats.c
@@ -1,1 +1,44 @@
 #include "../include/stats.h"
+/*
+PSEUDOCODE
+
+FUNCTION init_simstats()
+    
+    Allocate Memory for SimStats Function wrte adress in ptr_simstats
+
+    IF ptr_simstats == NULL (memory allocation failed)
+        OUTPUT Memory Allocation Filed
+        return NULL
+    END IF
+
+    set all field to 0 
+    return ptr_simstats
+END FUNCTION
+
+FUNCTION create_output_file(config)
+    
+
+
+    return ptr_output_file
+
+END FUNCTION
+
+FUNCTION update_simstats()
+    
+    
+
+    return ptr_output_file
+
+END FUNCTION
+
+FUNCTION save_temp_dataset()
+
+END FUNCTION
+
+
+FUNCTION save_final_dataset()
+
+END FUNCTION
+
+END PSEUDOCODE
+*/
