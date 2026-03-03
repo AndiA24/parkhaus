aa924ac46a18860fccf5a9f5bcdca075aadbd951
Lukas <lukas.kornmayer@web.de>
2026-03-03 17:03:03 +0100

    Add draft implementation of get_config function

--- src/config.c
+++ src/config.c
@@ -5,16 +5,26 @@ PSEUDOCODE
 
 FUNCTION create_config() RETURNS SimConfig*
     ptr_config : SimConfig*
-    ptr_config = CALL calloc(1, CALL sizeof *ptr_config)
+    ptr_config = CALL calloc(1, CALL sizeof *ptr_config)    //allocate memory with default values
 
-    IF ptr_config == NULL
+    IF ptr_config == NULL THEN                                   
         RETURN NULL
 
     RETURN ptr_config
 END FUNCTION
 
 
-FUNCTION get_config()
+FUNCTION get_config(ptr_simstats: SimStats*)
+    IF ptr_config == NULL THEN                                   
+        RETURN 
+
+    ptr_config_file : FILE*
+    ptr_config_file = CALL fopen(ptr_config->config_file_name, "r")
+
+    IF ptr_config_file == NULL THEN
+        RETURN
+
+    
 
 END FUNCTION
 
@@ -24,8 +34,12 @@ FUNCTION save_config()
 END FUNCTION
 
 
-FUNCTION free_config()
+FUNCTION free_config(ptr_config : SimConfig*)
 
+    IF ptr_config == NULL THEN                                   
+        RETURN 
+
+    CALL free(ptr_config)
 END FUNCTION
 
 */
