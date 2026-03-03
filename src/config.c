84f5c437c0cb91a500dfdc22e88dc770a3f24d24
Jorue <jost.ruething@gmail.com>
2026-03-03 17:41:29 +0100

    add save_config

--- src/config.c
+++ src/config.c
@@ -9,36 +9,59 @@ FUNCTION create_config() RETURNS SimConfig*
 
     IF ptr_config == NULL THEN                                   
         RETURN NULL
-
+    END IF
     RETURN ptr_config
 END FUNCTION
 
 
-FUNCTION get_config(ptr_simstats: SimStats*)
+FUNCTION get_config(SimConfig ptr_config)
     IF ptr_config == NULL THEN                                   
         RETURN 
+    END IF
 
     ptr_config_file : FILE*
     ptr_config_file = CALL fopen(ptr_config->config_file_name, "r")
 
     IF ptr_config_file == NULL THEN
         RETURN
+    END IF
 
-    
 
 END FUNCTION
 
 
-FUNCTION save_config()
+FUNCTION save_config(ptr_config : SimConfig*)
+    IF ptr_config == NULL THEN
+        RETURN
+    END IF
 
+    ptr_file : FILE*
+    ptr_file = CALL fopen(ptr_config->config_file_name, "w")
+
+    IF ptr_file == NULL THEN
+        RETURN
+    END IF
+
+    CALL fprintf(ptr_file, "%u,%u,%u,%u,%u,%u,%u,%s,%s,%u\n",
+        ptr_config->num_decks,
+        ptr_config->spots_per_deck,
+        ptr_config->initial_occupancy,
+        ptr_config->max_parking_duration_steps,
+        ptr_config->min_parking_duration_steps,
+        ptr_config->sim_duration_steps,
+        ptr_config->arrival_probability_percent,
+        ptr_config->output_file_name,
+        ptr_config->config_file_name,
+        ptr_config->seed)
+    CALL fclose(ptr_file)
 END FUNCTION
+
 
 
 FUNCTION free_config(ptr_config : SimConfig*)
-
     IF ptr_config == NULL THEN                                   
         RETURN 
-
+    END IF
     CALL free(ptr_config)
 END FUNCTION
 
