a822798b04e0889559befddbeb0c47a8f55810e5
Lukas <lukas.kornmayer@web.de>
2026-03-03 16:50:26 +0100

    Add create_config function to config.c

--- src/config.c
+++ src/config.c
@@ -3,8 +3,14 @@
 /*
 PSEUDOCODE
 
-FUNCTION create_config()
+FUNCTION create_config() RETURNS SimConfig*
+    ptr_config : SimConfig*
+    ptr_config = CALL calloc(1, CALL sizeof *ptr_config)
 
+    IF ptr_config == NULL
+        RETURN NULL
+
+    RETURN ptr_config
 END FUNCTION
 
 
