39b853682c575cb04aadb3b9f656ed1684a4827e
Jorue <jost.ruething@gmail.com>
2026-03-13 12:50:19 +0100

    add output function to utils to handle output incase of unit testing this still needs to be integrated actually

--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,21 @@
 #include "../include/utils.h"
 #include "../include/config.h"
+#ifndef UNIT_TEST
+#include "../include/ui.h"
+#endif
+#include <stdio.h>
 #include <stdlib.h>
 
+void output(int col, const char *ptr_msg, int color, int kill, SimConfig *ptr_config) {
+#ifdef UNIT_TEST
+    printf("%s\n", ptr_msg);
+#else
+    show_message(col, ptr_msg, color, kill, ptr_config);
+#endif
+}
+
 void quit(SimConfig *ptr_config) {
-    //save_config(ptr_config);
+    save_config(ptr_config);
     exit(1);
 }
 /*
